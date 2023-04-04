54dc392fdf7f4642922968b69c5fdcb8e32af180
thomasyonaha-mccoy <83908974+goat6@users.noreply.github.com>
2023-04-04 22:14:36 +0100

    accidental pull from main

--- rock-paper-scissors/main.js
+++ rock-paper-scissors/main.js
@@ -12,74 +12,83 @@ function getComputerChoice() {
 }
 
 function playRound(playerChoice, computerChoice) {
+    let winner = ""
     if (playerChoice === computerChoice) {
-        return "tie"
+        winner = "tie"
     } else if (playerChoice === "rock") {
         if (computerChoice === "paper") {
-            return "computer" 
+            winner = "computer"
         } else if (computerChoice === "scissors") {
-            return "player"
+            winner = "player"
         }
     } else if (playerChoice === "paper") {
         if (computerChoice === "scissors") {
-            return "computer"
+            winner = "computer"
         } else if (computerChoice === "rock") {
-            return "player"
+            winner = "player"
         }
     } else if (playerChoice === "scissors") {
         if (computerChoice === "rock") {
-            return "computer"
+            winner = "computer"
         } else if (computerChoice === "paper") {
-            return "player"
+            winner = "player"
         }
+    }
+    roundCount++
+    if (winner === "player") {
+        playerScore++
+        updateText(winner)
+    } else if (winner === "computer") {
+        computerScore++
+        updateText(winner)
+    } else if (winner === "tie") { 
+        gameStatus.textContent = `round ${roundCount} was a draw!`
     }
 }
 
-function promptUser() {
-    while (true) {
-        let userInput = prompt("State your choice: ")
-        if (userInput != null || userInput != undefined) {
-            userInput = userInput.toLowerCase() 
-        }
-        if (userInput === "rock" || userInput === "paper" || userInput === "scissors"|| userInput === "exit") {
-            return userInput
-        }
-        alert("Please enter a correct value of either rock, paper or scissors!")
+function checkForWinner() { 
+    if (computerScore === 5) { 
+        reset("computer");
+    } else if (playerScore === 5) { 
+        reset("player");
     }
 }
 
-function calculateOverallWinner(playerScore, computerScore) {
-    if (playerScore === computerScore) { 
-        return "The game is a draw."
+function updateText(winner,endComputerScore,endPlayerScore) {
+    winner = winner.charAt(0).toUpperCase()+winner.slice(1)
+    if (playerScore === 0 && computerScore === 0) {
+        gameStatus.textContent = `Last match was won by the ${winner}, the score was ${endPlayerScore} to the player and ${endComputerScore} to the computer. `
+    } else { 
+        gameStatus.textContent = `${winner} won round ${roundCount}`
     }
-    return (playerScore > computerScore ? "Player wins the game!" : "Computer wins this time.") // if playerScore > computer score return player wins else do opposite
+    computerCounter.textContent = computerScore
+    playerCounter.textContent = playerScore
 }
 
-function game() {
-    let playerScore = 0
-    let computerScore = 0
-    for (let roundCount = 0; roundCount < 5; roundCount++) {
-        let playerChoice = promptUser()
-        if (playerChoice === "exit") { 
-            console.log("Exiting game.")
-            return
-        }
-        let computerChoice = getComputerChoice()
-        let roundWinner = playRound(playerChoice, computerChoice)
-        console.log(`Player picked ${playerChoice}.`)
-        console.log(`Computer picked ${playerChoice}.`)
-        if (roundWinner === "player") {
-            playerScore += 1
-            console.log(`Player wins round ${roundCount+1}.`)
-        } else if (roundWinner === "computer") {
-            computerScore += 1
-            console.log(`Computer wins round ${roundCount+1}.`)
-        } else {
-            console.log("This round is a draw.")
-        }
-    }
-    console.log(calculateOverallWinner(playerScore,computerScore))
+function reset(winner) {
+    let endPlayerScore = playerScore
+    let endComputerScore = computerScore
+    computerScore = 0 
+    playerScore = 0
+    roundCount = 0
+    updateText(winner,endPlayerScore,endComputerScore)
 }
 
-console.log("Welcome to Rock, Paper, Scissors! Initialising game: ")
-game()
+function runGame() {
+    let selectedButton = this.classList.value
+    playRound(selectedButton, getComputerChoice())
+    checkForWinner()
+}
+
+let computerScore = 0
+let playerScore = 0
+let roundCount = 0
+const buttons = document.querySelectorAll("button")
+const gameStatus = document.querySelector(".gameStatus")
+const playerCounter = document.querySelector(".playerPoints")
+const computerCounter = document.querySelector(".computerPoints")
+const rockButton = document.querySelector('.rock')
+const paperButton = document.querySelector('.paper')
+const scissorsButton = document.querySelector('.scissors')
+
+buttons.forEach(button => button.addEventListener("click", runGame));
